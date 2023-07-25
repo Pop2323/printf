d33402b5ab118f07c004b3a30f011d6783a24da6
Pop2323 <omar4500455@gmail.com>
2023-07-24 19:30:40 -0700

    7_commit

--- print_rev.c
+++ print_rev.c
@@ -12,13 +12,13 @@
 int print_rev(va_list op)
 {
 	int i, j;
-	char *s = va_arg(op, char *);
+	char *str = va_arg(op, char *);
 
-	if (!s)
-		ss = "(null)";
-	for (i = 0; s[i]; i++)
-		;
+	if (!str)
+		str = "(null)";
+	while (str[i])
+		i++;
 	for (j = i - 1; j >= 0; j--)
-		_putchar(s[j]);
+		_putchar(str[j]);
 	return (i);
 }
