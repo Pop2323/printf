cb7f547bd8dfcba42640a51e701664c8c2b34b81
Pop2323 <omar4500455@gmail.com>
2023-07-24 22:55:18 -0700

    11_commit

--- _putchar.c
+++ _putchar.c
@@ -12,3 +12,19 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * _puts - func that print the str
+ *
+ * @str: ptr char
+ *
+ * Return: return the num of chars
+ */
+int _puts(char *str)
+{
+	register short i;
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	return (i);
+}
